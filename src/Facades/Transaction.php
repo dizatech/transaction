c50d088eeebf86f94ebad5a27fa432cef6730f08
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2021-01-19 13:06:47 +0330

    The BaseFacade and Transaction Facade added to package

---
+++ src/Facades/Transaction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dizatech\Transaction\Facades;
+
+/**
+ * @class \Dizatech\Transaction\Facades\Transaction
+ *
+ * @method static string driver(string $driver)
+ * @method static integer amount(integer $amount)
+ * @method static integer orderId(integer $orderId)
+ * @method static string callbackUrl(string $callbackUrl)
+ * @method static pay()
+ *
+ * @see \Dizatech\Transaction\Core\TransactionManager
+ */
+
+class Transaction extends BaseFacade
+{
+    //
+}
