072fca01247d2bdf53cb7df53f441e560b33cd9c
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2021-01-19 12:58:38 +0330

    The Core of Driver manager service created in package

--- src/Abstracts/Driver.php
+++ src/Abstracts/Driver.php
@@ -3,9 +3,12 @@
 namespace Dizatech\Transaction\Abstracts;
 
 use Dizatech\Transaction\Contracts\DriverInterface;
+use Dizatech\Transaction\Traits\DatabaseActions;
 
 abstract class Driver implements DriverInterface
 {
+    use DatabaseActions;
+
     public $driver;
 
     public function __construct($driver)
@@ -16,4 +19,8 @@ abstract class Driver implements DriverInterface
     abstract public function init($amount, $orderId, $callbackUrl,  $detail = []);
 
     abstract public function verify($request);
+
+    public function getInformation() {
+        return config('dizatech_transaction.information')[$this->driver]['constructor'];
+    }
 }
---
+++ src/Core/TransactionManager.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Dizatech\Transaction\Core;
+
+class TransactionManager
+{
+    protected $driver;
+    protected $amount;
+    protected $orderId;
+    protected $callbackUrl;
+    protected $detail;
+    protected $config;
+    protected $transaction;
+    protected $request;
+
+    public function __construct()
+    {
+        $this->config = config('dizatech_transaction');
+    }
+
+    public function pay()
+    {
+        $object = $this->fireDriver();
+        return $object->init($this->amount, $this->orderId, $this->callbackUrl, $this->detail);
+    }
+
+    public function verify()
+    {
+        $object = $this->fireDriver();
+        return $object->verify($this->request);
+    }
+
+    public function getDriver()
+    {
+        (is_null($this->driver))
+            ? $driver = $this->config['default']
+            : $driver = $this->driver;
+        return $driver;
+    }
+
+    public function fireDriver()
+    {
+        $class = $this->config['drivers'][$this->getDriver()];
+        return new $class($this->getDriver());
+    }
+
+    // Has parameter
+    public function driver($driver = null)
+    {
+        $this->driver = $driver;
+        return $this;
+    }
+
+    public function amount($amount)
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function orderId($orderId)
+    {
+        $this->orderId = $orderId;
+        return $this;
+    }
+
+    public function callbackUrl($callbackUrl)
+    {
+        $this->callbackUrl = $callbackUrl;
+        return $this;
+    }
+
+    public function request($request)
+    {
+        $this->request = $request;
+        return $this;
+    }
+
+    public function detail($detail = [])
+    {
+        $this->detail = $detail;
+        return $this;
+    }
+}
