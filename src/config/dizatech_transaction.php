ab10476e79fad636049c8fc629a6f08a57a82547
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2021-01-19 13:11:17 +0330

    The dizatech_transaction config file was added to package

---
+++ src/config/dizatech_transaction.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Driver
+    |--------------------------------------------------------------------------
+    |
+    | This value determines which of the following gateway to use.
+    | You can switch to a different driver at runtime.
+    |
+    */
+    'default' => 'parsian',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Drivers Information
+    |--------------------------------------------------------------------------
+    |
+    | These are the list of drivers information to use in package.
+    | You can change the information.
+    |
+    */
+    'information' => [
+        'parsian' => [
+            'constructor' => [
+                'pin' => ''
+            ],
+            'options' => [
+
+            ]
+        ],
+        'pasargad' => [
+            'constructor' => [
+                'merchant_code' => '',
+                'terminal_code' => '',
+                'private_key'   => ''
+            ],
+            'options' => [
+                'verifySSL' => FALSE
+            ]
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | List of Drivers
+    |--------------------------------------------------------------------------
+    |
+    | These are the list of drivers to use for this package.
+    | You can change the name.
+    |
+    */
+    'drivers' => [
+        'parsian' => \Dizatech\Transaction\Drivers\Parsian::class,
+        'pasargad' => \Dizatech\Transaction\Drivers\Pasargad::class
+    ],
+];
