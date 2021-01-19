034038d104f3973e86c00bf070d040a5d52ceb75
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2021-01-19 12:47:13 +0330

    The migration file and Service Provider added

---
+++ src/TransactionServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dizatech\Transaction;
+
+use Illuminate\Support\ServiceProvider;
+
+class TransactionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+    }
+}
---
+++ src/database/migrations/2021_01_11_000000_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->decimal('amount', 15,2);
+            $table->string('driver', 128);
+            $table->enum('status', ['pending', 'successful', 'failed', 'refunded']);
+            $table->string('ref_no')->nullable();
+            $table->string('token', 512)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
