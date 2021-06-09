d3757c40819f680937c427c87f38a0775f34017b
Sina <sina.rajabpour@outlook.com>
2021-06-09 14:22:09 +0430

    Added gateway, toman and status_label accessors in transaction model

--- src/Models/Transaction.php
+++ src/Models/Transaction.php
@@ -10,9 +10,53 @@ class Transaction extends Model
     use HasFactory;
 
     protected $fillable = ['order_id', 'status', 'amount', 'driver', 'ref_no', 'token'];
+    protected $appends = ['gateway','toman','status_label'];
 
     public function parent()
     {
         return $this->belongsTo( config('dizatech_transaction.model'), 'order_id' );
+    }
+
+    public function getGatewayAttribute()
+    {
+        switch ($this->driver) {
+            case 'pasargad':
+                $label = 'بانک پاسارگاد';
+                break;
+            case 'parsian' :
+                $label = 'بانک پارسیان';
+                break;
+            default:
+                $label = 'نامشخص';
+
+        }
+        return $label;
+    }
+
+    public function getTomanAttribute()
+    {
+        return $this->amount / 10;
+    }
+
+    public function getStatusLabelAttribute()
+    {
+        switch ($this->status) {
+            case 'pending':
+                $label = 'در انتظار پرداخت';
+                break;
+            case 'failed':
+                $label = 'ناموفق';
+                break;
+            case  'refunded':
+                $label = 'برگشت خورده';
+                break;
+            case 'successful':
+                $label = 'موفقیت‌آمیز';
+                break;
+            case 'default':
+                $label = 'نامشخص';
+                break;
+        }
+        return $label;
     }
 }
